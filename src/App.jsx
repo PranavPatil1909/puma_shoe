37885f60cd04a64cab87fdb36d651c1f83c3e3da
PranavPatil1909 <109128442+PranavPatil1909@users.noreply.github.com>
2023-10-16 13:56:57 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,22 @@ import Floor from './Floor'
 
 export default function App() {
     return (
-        <Canvas shadows camera={{ position: [-0.3, 0.2, -0.4] }}>
-            <Environment preset="sunset" background blur={[0.1]} />
+        <Canvas shadows camera={{ position: [-0.3, 0.4, -0.4] }}>
+            <Environment preset="lobby"
+                background
+            // blur={[0.1]}
+            />
+
             {/* <Environment preset="city" /> */}
-            <gridHelper args={[2, 2, 0xff0000, 'BLACK']} />
+            {/* <gridHelper args={[2, 2, 0xff0000, 'BLACK']} /> */}
 
-            <axesHelper args={[5]} />
+            {/* <axesHelper args={[5]} /> */}
             <Floor />
-            <ambientLight intensity={0.7} />
+            <ambientLight intensity={0.5}    />
 
 
 
-            <spotLight intensity={0.5} angle={0.1} position={[0, 4.5, 0]} castShadow color={'white'} />
+            <spotLight intensity={1.5} angle={0.1} position={[-9, 20, 6]} castShadow color={'white'} />
             <Model />
 
 
--- src/Floor.jsx
+++ src/Floor.jsx
@@ -1,25 +1,28 @@
-import { MeshReflectorMaterial } from '@react-three/drei'
+import { MeshReflectorMaterial, Stage } from '@react-three/drei'
 
 export default function Floor() {
     return (
         <mesh rotation-x={-Math.PI / 2} receiveShadow>
-            <circleGeometry args={[2]} />
-         
+            <circleGeometry args={[3]} />
+
             <MeshReflectorMaterial
+                
                 blur={[5, 5]} // Blur ground reflections (width, heigt), 0 skips blur
                 mixBlur={0.75} // How much blur mixes with surface roughness
                 mixStrength={0.5} // Strength of the reflections
-                resolution={1000} // Off-buffer resolution, lower=faster, higher=better quality
+                resolution={1024} // Off-buffer resolution, lower=faster, higher=better quality
                 args={[2, 2]} // PlaneBufferGeometry arguments
                 rotation={[-Math.PI * 0.5, 0, 0]}
-                mirror={0.5} // Mirror environment, 0 = texture colors, 1 = pick up env colors
+                mirror={0.1} // Mirror environment, 0 = texture colors, 1 = pick up env colors
                 minDepthThreshold={0.25}
                 maxDepthThreshold={1}
                 depthScale={50}
                 receiveShadow
-                metalness={0.9} roughness={0.9} />
-           
-            
+                distortion={[0]}
+                metalness={0.9} roughness={0}
+            />
+
+
         </mesh>
     )
 }
