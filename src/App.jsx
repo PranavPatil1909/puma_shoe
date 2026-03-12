0e2715cd8343a3d7514d536f1e5f53e7888583c4
26akshaypatil92 <26akshaypatil92@gmail.com>
2026-03-12 14:45:52 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,71 @@
 import { Canvas, extend } from '@react-three/fiber'
-import { OrbitControls, Environment, Reflector, MeshReflectorMaterial, useProgress, Html } from '@react-three/drei'
-import { Suspense } from 'react';
-import { useState, CSSProperties } from "react";
-import WasdControls from './Wasd-contorls';
+import {
+  OrbitControls,
+  Environment,
+  Reflector,
+  MeshReflectorMaterial,
+  useProgress,
+  Html
+} from '@react-three/drei'
+import { Suspense } from 'react'
+import { useState, CSSProperties } from 'react'
+import WasdControls from './Wasd-contorls'
 import Loading from './Loader'
-
-
 
 // extend({ Reflector });
 
-
 import { Model } from './Shoe'
 import Floor from './Floor'
-import { Spinner } from './Spinner';
-
+import { Spinner } from './Spinner'
+import { Cabinet } from './cupboard'
 
 export default function App() {
-    let [loading, setLoading] = useState(true);
-    let [color, setColor] = useState("#ffffff");
-    return (
-        <>
+  let [loading, setLoading] = useState(true)
+  let [color, setColor] = useState('#ffffff')
+  return (
+    <>
+      <Canvas shadows camera={{ position: [0, 3, 5] }}>
+        <Suspense>
+          <Environment
+            preset="dawn"
+            background
+            backgroundBlurriness={1}
+            backgroundIntensity={1}
+            environmentRotation={[0, Math.PI / 2, 0]}
+            // blur={[0.1]}
+            // ground={{
+            //   height: 15,
+            //   radius: 115,
+            //   scale: 100
+            // }}
+          />
 
-            <Canvas shadows camera={{ position: [-0.3, 0.4, -0.4] }}>
-                <Suspense >
+          {/* <Environment preset="city" /> */}
+          {/* <gridHelper args={[2, 2, 0xff0000, 'BLACK']} /> */}
 
-                    <Environment preset="lobby"
-                        background
-                        // blur={[0.5]}
-                        ground={{
-                            height: 15,
-                            radius: 115,
-                            scale: 100
-                        }}
-                    />
+          {/* <axesHelper args={[5]} /> */}
+          {/* <Floor /> */}
+          <ambientLight intensity={0.5} />
 
-                    {/* <Environment preset="city" /> */}
-                    {/* <gridHelper args={[2, 2, 0xff0000, 'BLACK']} /> */}
+          <spotLight
+            intensity={1.9}
+            angle={0.1}
+            position={[-9, 9, 6]}
+            // position={[0, 2, 0]}
 
-                    {/* <axesHelper args={[5]} /> */}
-                    <Floor />
-                    <ambientLight intensity={0.5} />
+            castShadow
+            color={'white'}
+          />
 
+          {/* <Model /> */}
+          <group position={[0, 2, 1]}>
+            <Cabinet />
+          </group>
 
-
-                    <spotLight
-                        intensity={1.9} angle={0.1}
-                        position={[-9, 9, 6]}
-                        // position={[0, 2, 0]}
-
-                        castShadow color={'white'} />
-
-
-
-                    <Model />
-
-
-
-                    <OrbitControls
-                        enableZoom={true} maxPolarAngle={Math.PI / 2.5}
-                    />
-                    <WasdControls />
-                </Suspense>
-
-            </Canvas>
-
-
-
-
-        </>
-    )
+          <OrbitControls enableZoom={true} target={[0, 4, 0]} />
+          <WasdControls />
+        </Suspense>
+      </Canvas>
+    </>
+  )
 }
---
+++ src/cupboard.jsx
@@ -0,0 +1,128 @@
+
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+
+export function Cabinet(props) {
+  const { nodes, materials } = useGLTF('https://models-kosoku-3d.s3.ap-south-1.amazonaws.com/MODA-7_Baked_Maps_Attached.glb')
+  return (
+    <group {...props} dispose={null}>
+      <group position={[0, 1.067, -1.773]} scale={[1.745, 3.5, 0.25]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube004_1.geometry}
+          material={materials['Stainles Steel']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube004_2.geometry}
+          material={materials['BLACK RAL.003']}
+        />
+      </group>
+      <group position={[0, -0.007, -0.868]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube002_1.geometry}
+          material={materials['Basalt_Black.001']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube002_2.geometry}
+          material={materials.Balck_Ral}
+        />
+      </group>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Ceiling.geometry}
+        material={materials.Above_Fake_Ceiling}
+        position={[0, 3.074, -0.868]}
+      />
+      <group position={[1.071, 1.617, -0.28]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube109.geometry}
+          material={materials['BLACK RAL']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube109_1.geometry}
+          material={materials.Wood}
+        />
+      </group>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.stainless_steel_.geometry}
+        material={materials['Stainles Steel']}
+        position={[0.988, 0.854, -0.868]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Base_Side_R001.geometry}
+        material={materials['Stainles Steel']}
+        position={[1.071, 0.083, -0.868]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.wood004.geometry}
+        material={materials['BLACK RAL']}
+        position={[0, 1.617, -0.841]}
+        rotation={[-Math.PI, 0, 0]}
+        scale={-1}
+      />
+      <group position={[-0.002, 2.909, -0.878]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube026.geometry}
+          material={materials['Black matte metal']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube026_1.geometry}
+          material={materials['BLACK RAL']}
+        />
+      </group>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube011.geometry}
+        material={materials.E_3000K}
+        position={[0.938, 2.948, -0.509]}
+        scale={[1.172, 1.172, 5.281]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube002.geometry}
+        material={materials.E_3000K}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube003.geometry}
+        material={materials.E_3000K}
+        position={[0.206, 2.948, -1.634]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube004.geometry}
+        material={materials.E_3000K}
+        position={[0.206, 2.948, -0.037]}
+      />
+    </group>
+  )
+}
+
+useGLTF.preload('https://models-kosoku-3d.s3.ap-south-1.amazonaws.com/MODA-7_Baked_Maps_Attached.glb')
